48b238da2d490ccb8219b192244dbbdb086976a1
sakurafunctional <sakurafunctional@gmail.com>
2015-04-16 15:13:10 +0900

    commit

--- test.js
+++ test.js
@@ -4,21 +4,14 @@ var ___ = require('./worldcomponent');
 
 var ___a = ___(0);
 var ___b = ___(0);
-var ___c = ___(0);
 
 ___.world = ___a.compute(___.log('a:'));
 ___.world = ___b.compute(___.log('b:'));
-___.world = ___c.compute(___.log('c:'));
 
 ___.world = ___a.compute(function(x)
 {
   ___.world = ___b.appear(x * 5);
 });
-___.world = ___b.compute(function(x)
-{
-  ___.world = ___c.appear(x * 2);
-});
-
 
 var f = function()
 {
